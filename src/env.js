643eabfd7ad81c3e0ed1f736ecb0b0dc8fc1c045
Nald Ozem Aguilar <naldoz@naldouche.local>
2025-09-22 19:13:24 -0700

    refactor: environment variables

--- src/env.js
+++ src/env.js
@@ -11,6 +11,10 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
+    CLERK_SECRET_KEY: z.string(),
   },
 
   /**
@@ -29,6 +33,9 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
--- src/server/database/redisClient.ts
+++ src/server/database/redisClient.ts
@@ -1,9 +1,9 @@
 import { Redis } from "@upstash/redis";
-import "dotenv/config";
+import { env } from "../../env";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+export const redis = new Redis({
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default redis;
