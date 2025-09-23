81658e73b065fd1f8e620d1f23f1f71b7c7bbd0d
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-09-23 12:36:34 -0700

    redis & postgres connections, local & hosted with auto-switching

--- src/server/database/redisClient.ts
+++ src/server/database/redisClient.ts
@@ -1,9 +1,28 @@
-import { Redis } from "@upstash/redis";
+import { Redis as UpstashRedis } from "@upstash/redis";
 import "dotenv/config";
+import { createClient } from "redis";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+import { env } from "@/env";
+
+const createLocalRedisClient = () => {
+  const client = createClient({ url: env.REDIS_URL });
+
+  client.on("error", (err) => {
+    console.error("Redis client error", err);
+  });
+
+  client
+    .connect()
+    .catch((err) => console.error("Unable to connect to Redis", err));
+
+  return client;
+};
+
+const redis = env.PRODUCTION
+  ? new UpstashRedis({
+      url: env.UPSTASH_REDIS_REST_URL!,
+      token: env.UPSTASH_REDIS_REST_TOKEN!,
+    })
+  : createLocalRedisClient();
 
 export default redis;
