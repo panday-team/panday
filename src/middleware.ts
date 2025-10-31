0e690e0af17e34fb0925b987a6ab25193ee5db1a
hateNaldoz <aguilarnald@gmail.com>
2025-10-31 13:53:23 -0700

    refactor: use userId as ratelimit key

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-<<<<<<< HEAD
-const isProtectedRoute = createRouteMatcher(["/roadmap(.*)", "/profile(.*)"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  // Protect authenticated routes
-=======
 const isProtectedRoute = createRouteMatcher(["/roadmap(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
->>>>>>> 089acaa (changed roadmap to be a protected route & added simple homepage)
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
