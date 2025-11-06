d8db23e095bb1ceb3727462e644460980e932088
hateNaldoz <aguilarnald@gmail.com>
2025-11-05 22:41:50 -0800

    add: automatic scrolling on message send

--- src/components/chat/chat-loading.tsx
+++ src/components/chat/chat-loading.tsx
@@ -1,9 +1,26 @@
 export default function ChatLoading() {
   return (
+<<<<<<< HEAD
     <div className="flex items-center gap-1.5 text-xs">
       <span className="inline-block h-1.5 w-1.5 animate-bounce rounded-full bg-gray-600 [animation-delay:-0.3s] dark:bg-white/60"></span>
       <span className="inline-block h-1.5 w-1.5 animate-bounce rounded-full bg-gray-600 [animation-delay:-0.15s] dark:bg-white/60"></span>
       <span className="inline-block h-1.5 w-1.5 animate-bounce rounded-full bg-gray-600 dark:bg-white/60"></span>
     </div>
+=======
+    <ul className="flex w-[60px] justify-evenly">
+      <li
+        className="animate-chat-bounce h-3 w-3 list-none rounded-full bg-white"
+        aria-hidden="true"
+      />
+      <li
+        className="animate-chat-bounce h-3 w-3 list-none rounded-full bg-white [animation-delay:0.3s]"
+        aria-hidden="true"
+      />
+      <li
+        className="animate-chat-bounce h-3 w-3 list-none rounded-full bg-white [animation-delay:0.6s]"
+        aria-hidden="true"
+      />
+    </ul>
+>>>>>>> 1218528 (add: automatic scrolling on message send)
   );
 }
